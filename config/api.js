6ff7a4c85f09397398a080c257a30d59ace640d3
Fandi Nur <fandinur@192.168.1.24>
2023-01-06 01:42:17 +0700

    deploy

---
+++ config/api.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import { ApiError } from 'next/dist/server/api-utils';
+
+export const API = axios.create({
+    // baseURL:"https://waysfoodbe.herokuapp.com/api/v1",
+    baseURL:"https://waysfood-api.up.railway.app/api/v1",
+});
+
+export const setAuthToken =(token)=>{
+    if(token){
+        API.defaults.headers.common["Authorization"] =`Bearer ${token}`;
+    } else{
+        delete API.defaults.headers.common["Authorization"];
+    }
+};
